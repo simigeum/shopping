551cea3764fcaa39f31eb31e0b4ecc3ad40829f6
simigeum <ldu50@naver.com>
2021-11-16 23:35:56 +0900

    ✨ React-Bootstrap installs and Badge component tests

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button, Badge} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button variant="primary">
+          Profile <Badge bg="secondary">9</Badge>
+          <span className="visually-hidden">unread messages</span>
+       </Button>
     </div>
   );
 }
