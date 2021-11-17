7f55cc7ff4b839e23f5cac317223fa58b9afd760
simigeum <ldu50@naver.com>
2021-11-17 20:20:21 +0900

    🚧 Use Navbar, Carousel, grid-system of React-Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,128 @@
-import './App.css';
-import {Button, Badge} from 'react-bootstrap';
+import './App.css'
+import {
+  Button,
+  Navbar,
+  Offcanvas,
+  NavDropdown,
+  Container,
+  Form,
+  FormControl,
+  Nav,
+  Carousel,
+} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">
-          Profile <Badge bg="secondary">9</Badge>
-          <span className="visually-hidden">unread messages</span>
-       </Button>
+      <Navbar bg="light" expand={false}>
+        <Container fluid>
+          <Navbar.Brand href="#">LOCO</Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar"
+            aria-labelledby="offcanvasNavbarLabel"
+            placement="end"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action5">
+                    Something else here
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
+      <Carousel variant="dark">
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://placehold.it/800x400?text=First slide&bg=f5f5f5"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h5>First slide label</h5>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://placehold.it/800x400?text=Second slide&bg=eee"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h5>Second slide label</h5>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="http://placehold.it/800x400?text=Third slide&bg=e5e5e5"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h5>Third slide label</h5>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="100%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="100%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="100%"
+            />
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </div>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
